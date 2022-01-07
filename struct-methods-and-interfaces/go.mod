4850a4d957db1f1f833be8e8536d7cb4a58be935
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2022-01-07 21:24:54 +0700

    Add the TDD explanation for struct, methods, and interfaces

---
+++ struct-methods-and-interfaces/go.mod
@@ -0,0 +1,3 @@
+module struct-methods-and-interfaces
+
+go 1.17
---
+++ struct-methods-and-interfaces/shapes.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"math"
+)
+
+type Shape interface {
+	Perimeter() float64
+	Area() float64
+}
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.Height + r.Height)
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func (c Circle) Perimeter() float64 {
+	return 2 * math.Pi * c.Radius
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
+}
+
+func (t Triangle) Perimeter() float64 {
+	return 0
+}
