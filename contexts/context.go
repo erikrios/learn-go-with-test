59a22944c2595402220508dccb6b3fd4f88de0f5
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2022-01-18 19:53:34 +0700

    Add the example of context

--- contexts/context.go
+++ contexts/context.go
@@ -1,16 +1,23 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 )
 
 type Store interface {
-	Fetch() string
+	Fetch(ctx context.Context) (string, error)
 }
 
 func Server(store Store) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, store.Fetch())
+		data, err := store.Fetch(r.Context())
+
+		if err != nil {
+			return // todo: log error however you like
+		}
+
+		fmt.Fprint(w, data)
 	}
 }
